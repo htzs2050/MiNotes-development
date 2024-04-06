79dfc89f4d114aecb1f3965e0c76668f888d5e7e
htzs2050 <15298955526@163.com>
2024-04-06 22:26:26 +0800

    添加了一个功能，用于在编辑笔记时添加图片

--- app/src/main/java/net/micode/notes/model/WorkingNote.java
+++ app/src/main/java/net/micode/notes/model/WorkingNote.java
@@ -38,7 +38,8 @@ public class WorkingNote {
     // Note Id
     private long mNoteId;
     // Note content
-    private String mContent;
+//    private String mContent; //修改0304
+    public String mContent;
     // Note mode
     private int mMode;
 
